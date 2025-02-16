67285c54240cfdc1253d37115b63f2c876f781fd
Parit Bhardwaj <paritbhardwaj@outlook.com>
2025-02-16 12:19:10 +0530

    feat: backup

--- src/scripts/backups/attendanceBackup.js
+++ src/scripts/backups/attendanceBackup.js
@@ -7,16 +7,82 @@ const prisma = new PrismaClient();
 async function backupAttendanceData() {
   try {
     const labours = await prisma.labour.findMany({
-      include: {
-        user: true,
-        contractor: {
-          include: {
-            user: true,
+      select: {
+        id: true,
+        fingerprint_data: true,
+        aadhar_number: true,
+        employeeNo: true,
+        createdById: true,
+        updatedById: true,
+        createdAt: true,
+        updatedAt: true,
+        user: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+            mobile_number: true,
+            roleId: true,
+            role: true,
+            createdAt: true,
+            updatedAt: true,
+            password: true,
           },
         },
-        photos: true,
-        pdfs: true,
-        attendance: true,
+        contractor: {
+          select: {
+            id: true,
+            firm_name: true,
+            employeeNo: true,
+            managerId: true,
+            aadhar_number: true,
+            startDate: true,
+            endDate: true,
+            siteCode: true,
+            createdAt: true,
+            updatedAt: true,
+            user: {
+              select: {
+                id: true,
+                name: true,
+                username: true,
+                mobile_number: true,
+                roleId: true,
+                role: true,
+                createdAt: true,
+                updatedAt: true,
+                password: true,
+              },
+            },
+          },
+        },
+        photos: {
+          select: {
+            id: true,
+            url: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        pdfs: {
+          select: {
+            id: true,
+            url: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+        attendance: {
+          select: {
+            id: true,
+            inTime: true,
+            outTime: true,
+            workingHours: true,
+            date: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
       },
     });
 
